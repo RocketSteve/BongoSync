3bc4e5c69e088c15c84885e6937c55da087c731b
RocketSteve <simoszy@gmail.com>
2023-12-13 09:10:06 +0100

    HandleRegister.cpp Revised communication with server for updated ServerCommunicator

--- Client/include/HandleRegister.h
+++ Client/include/HandleRegister.h
@@ -11,13 +11,7 @@ class HandleRegister {
 public:
     void initiateRegistration();
 private:
-    std::string promptForEmail();
-    std::string promptForPassword();
-    std::string getHostname();
-    bool confirmPassword(const std::string& password);
-
     bool checkWithServer(const std::string& email, const std::string& password, const std::string& hostname);
-
     void createConfigFile(const std::string& email, const std::string& hostname);
 };
 
--- Client/src/HandleRegister.cpp
+++ Client/src/HandleRegister.cpp
@@ -1,17 +1,18 @@
 #include "../include/HandleRegister.h"
 #include "../include/ServerCommunicator.h"
 #include "../include/MessageCreator.h"
+#include "../include/Utility.h"
 #include <iostream>
-#define HOST_NAME_MAX 255
 
 void HandleRegister::initiateRegistration() {
-    std::string email = promptForEmail();
+
+    std::string email = Utility::promptForEmail();
     std::string password;
-    std::string hostname = getHostname();
+    std::string hostname = Utility::getHostname();
 
     do {
-        password = promptForPassword();
-    } while (!confirmPassword(password));
+        password = Utility::promptForPassword();
+    } while (!Utility::confirmPassword(password));
 
     if (checkWithServer(email, password, hostname)) {
         std::cout << "User already exists, please try again.\n";
@@ -21,80 +22,20 @@ void HandleRegister::initiateRegistration() {
     }
 }
 
-std::string HandleRegister::promptForEmail() {
-    std::string email;
-    std::cout << "Enter your email: ";
-    std::getline(std::cin, email);
-    return email;
-}
-
-std::string HandleRegister::promptForPassword() {
-    std::string password;
-    std::cout << "Enter your password: ";
-
-    // Turning off terminal echoing
-    termios oldt;
-    tcgetattr(STDIN_FILENO, &oldt);
-    termios newt = oldt;
-    newt.c_lflag &= ~ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-
-    std::getline(std::cin, password);
-
-    // Restore terminal settings
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-
-    std::cout << std::endl;
-    return password;
-}
-
-bool HandleRegister::confirmPassword(const std::string& password) {
-    std::string confirmPassword;
-    std::cout << "Confirm your password: ";
-
-    // Turning off terminal echoing
-    termios oldt;
-    tcgetattr(STDIN_FILENO, &oldt);
-    termios newt = oldt;
-    newt.c_lflag &= ~ECHO;
-    tcsetattr(STDIN_FILENO, TCSANOW, &newt);
-
-    std::getline(std::cin, confirmPassword);
-
-    // Restore terminal settings
-    tcsetattr(STDIN_FILENO, TCSANOW, &oldt);
-    std::cout << std::endl;
-
-    if (password != confirmPassword) {
-        std::cout << "Passwords do not match, please try again.\n";
-        return false;
-    }
-    return true;
-}
-
-std::string HandleRegister::getHostname() {
-    char hostname[HOST_NAME_MAX];
-    if (gethostname(hostname, sizeof(hostname)) == -1) {
-        std::cerr << "Error getting hostname: " << std::strerror(errno) << '\n';
-        return "unknown";
-    }
-    return std::string(hostname);
-}
-
 bool HandleRegister::checkWithServer(const std::string& email, const std::string& password, const std::string& hostname) {
     // Implementation of checking with server
     std::cout << "Checking with server ...\n" << email << "\n" << password << "\n" << hostname << "\n";
 
     bool response;
-    ServerCommunicator serverCommunicator("10.0.0.420", 420);
-    if (serverCommunicator.connectToServer()) {
-        MessageCreator messageCreator;
-        std::string message = messageCreator.createRegistratioMessage(email, password, hostname);
-        response = serverCommunicator.sendMessage(message);
-    } else {
+    auto& communicator = ServerCommunicator::getInstance();
+    if (!communicator.isConnectedToServer() && !communicator.connectToServer()) {
         std::cerr << "Failed to connect to server\n";
         return false;
     }
+
+    MessageCreator messageCreator;
+    std::string message = messageCreator.createRegistratioMessage(email, password, hostname);
+    response = communicator.sendMessage(message);
 
     if (response) {
         return true;
@@ -106,7 +47,7 @@ bool HandleRegister::checkWithServer(const std::string& email, const std::string
 void HandleRegister::createConfigFile(const std::string& email, const std::string& hostname) {
     const std::string filesDir = std::filesystem::current_path();
     const std::string configDir = std::filesystem::path(getenv("HOME")) / ".bongo";
-    const std::string configFile = configDir / "config.txt";
+    const std::string configFile = configDir + "/config.json";
 
     // Create the hidden directory if it does not exist
     std::filesystem::create_directories(configDir);
