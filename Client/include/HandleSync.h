a0a58fd425bc1c3e68d761012bb5a610701a43f6
RocketSteve <simoszy@gmail.com>
2023-12-14 11:48:50 +0100

    Updated HandleSync to use MessageBuilder TODO complete processSErverSyncResponse method (maybe turn into class)

--- Client/include/HandleSync.h
+++ Client/include/HandleSync.h
@@ -1,13 +1,21 @@
 #ifndef CLIENT_HANDLESYNC_H
 #define CLIENT_HANDLESYNC_H
 
+#include "../include/Utility.h"
+#include "../include/ServerCommunicator.h"
+#include "../include/MessageBuilder.h"
+#include "../include/MessageCreator.h"
+#include <iostream>
+
+
 class HandleSync {
 public:
     void initiateSync();
 private:
     void checkWithServer();
-    void syncWithServer();
-    void locConflict();
+    bool checkWithServer(std::string& treeHash, std::string& hostname);
+    void locateConflict();
+    void processServerSyncResponse(const std::string& response);
 };
 
 #endif //CLIENT_HANDLESYNC_H
--- Client/src/HandleSync.cpp
+++ Client/src/HandleSync.cpp
@@ -1,16 +1,59 @@
 #include "../include/HandleSync.h"
-#include <iostream>
 
 void HandleSync::initiateSync() {
-    std::string email = promptForEmail();
-    std::string password = promptForPassword();
-    std::string hostname = getHostname();
 
-    if (checkWithServer(email, password, hostname)) {
+    std::string hostname = Utility::getHostname();
+    std::string treeHash = Utility::getTreeHash();
+
+    if (checkWithServer(treeHash, hostname)) {
         std::cout << "User does not exist, please try again.\n";
         initiateSync();
     } else {
         std::cout << "Syncing ...\n";
         std::cout << "Sync complete.\n";
+    }
+}
+
+bool HandleSync::checkWithServer(std::string& treeHash, std::string& hostname) {
+    // Create a message to request synchronization data using MessageBuilder
+    std::string syncRequestMsg = MessageCreator::create()
+            .setTreeHash(treeHash)
+            .setHostname(hostname)
+            .buildAskIfLatestMessage();
+
+    // Get the instance of ServerCommunicator
+    auto& serverCommunicator = ServerCommunicator::getInstance();
+    if (!serverCommunicator.isConnectedToServer() && !serverCommunicator.connectToServer()) {
+        std::cerr << "Failed to connect to server for synchronization.\n";
+        return 0;
+    }
+
+    // Send the synchronization request message
+    if (!serverCommunicator.sendMessage(syncRequestMsg)) {
+        std::cerr << "Failed to send synchronization request.\n";
+        return 0;
+    }
+
+    // Example: Receive response from the server
+    std::string serverResponse;
+    if (!serverCommunicator.receiveMessage(serverResponse)) {
+        std::cerr << "Failed to receive response from server.\n";
+        return 0;
+    }
+
+    // Process the server's response
+    // Implementation depends on your server's protocol and data format
+    processServerSyncResponse(serverResponse);
+    return 1;
+}
+
+void HandleSync::processServerSyncResponse(const std::string& response) {
+    // Example processing (you'll need to replace this with actual logic)
+    if (response == "SYNC_NEEDED") {
+        // Perform synchronization actions
+    } else if (response == "NO_SYNC_REQUIRED") {
+        // No action needed
+    } else {
+        // Handle other cases or errors
     }
 }
