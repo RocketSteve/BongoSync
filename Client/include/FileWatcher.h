a95ae003da3fff22937b84cf409b3e3b715cb358
RocketSteve <simoszy@gmail.com>
2023-12-14 11:54:21 +0100

    Turned fileWatcher into singleton

--- Client/include/FileWatcher.h
+++ Client/include/FileWatcher.h
@@ -4,26 +4,30 @@
 #include <uv.h>
 #include <string>
 #include <functional>
-#include <thread>
-#include <atomic>
+#include "../include/CollisionDetector.h"
+
+class CollisionDetector;  // Forward declaration
 
 class FileWatcher {
 public:
-    FileWatcher(const std::string& path, std::function<void(const std::string&)> onChange);
-    ~FileWatcher();
+    static FileWatcher& getInstance();
+    FileWatcher(const FileWatcher&) = delete;            // Delete copy constructor
+    FileWatcher& operator=(const FileWatcher&) = delete; // Delete copy assignment operator
 
-    void start();
+    void start(const std::string& path, CollisionDetector& collisionDetector);
     void stop();
 
 private:
+    FileWatcher();  // Private constructor
+    ~FileWatcher(); // Destructor
+
     static void onFileChange(uv_fs_event_t* handle, const char* filename, int events, int status);
 
-    uv_fs_event_t fsEvent;
     uv_loop_t* loop;
+    uv_fs_event_t fsEvent;
     std::string directoryToWatch;
-    std::function<void(const std::string&)> onChangeCallback;
-    std::thread watcherThread;
-    std::atomic<bool> running;
+    CollisionDetector* collisionDetector;
+    bool isRunning;
 };
 
 #endif // FILEWATCHER_H
--- Client/src/FileWatcher.cpp
+++ Client/src/FileWatcher.cpp
@@ -1,55 +1,46 @@
 #include "../include/FileWatcher.h"
-#include <iostream>
 
-FileWatcher::FileWatcher(const std::string& path, std::function<void(const std::string&)> onChange)
-        : directoryToWatch(path), onChangeCallback(onChange), running(false) {
-    loop = uv_default_loop();
-    uv_fs_event_init(loop, &fsEvent);
-}
+
+FileWatcher::FileWatcher() : loop(uv_default_loop()), collisionDetector(nullptr), isRunning(false) {}
 
 FileWatcher::~FileWatcher() {
     stop();
 }
 
-void FileWatcher::start() {
-    running = true;
-    watcherThread = std::thread([this]() {
-        fsEvent.data = this;
-        uv_fs_event_start(&fsEvent, FileWatcher::onFileChange, directoryToWatch.c_str(), 0);
-        uv_run(loop, UV_RUN_DEFAULT);
-    });
+FileWatcher& FileWatcher::getInstance() {
+    static FileWatcher instance;
+    return instance;
+}
+
+void FileWatcher::start(const std::string& path, CollisionDetector& detector) {
+    if (isRunning) {
+        return; // Already running
+    }
+
+    directoryToWatch = path;
+    collisionDetector = &detector;
+
+    uv_fs_event_init(loop, &fsEvent);
+    fsEvent.data = this;
+    uv_fs_event_start(&fsEvent, FileWatcher::onFileChange, directoryToWatch.c_str(), 0);
+    uv_run(loop, UV_RUN_DEFAULT);
+
+    isRunning = true;
 }
 
 void FileWatcher::stop() {
-    if (running) {
-        running = false;
-        uv_stop(loop);
-
-        if (watcherThread.joinable()) {
-            watcherThread.join();
-        }
-        uv_fs_event_stop(&fsEvent);
-        uv_loop_close(loop);
+    if (!isRunning) {
+        return; // Not running
     }
+
+    uv_fs_event_stop(&fsEvent);
+    isRunning = false;
 }
 
 void FileWatcher::onFileChange(uv_fs_event_t* handle, const char* filename, int events, int status) {
     FileWatcher* watcher = static_cast<FileWatcher*>(handle->data);
-    if (watcher && filename) {
-        watcher->onChangeCallback(filename);
+    if (watcher && watcher->collisionDetector && filename) {
+        // Notify the CollisionDetector class about the change
+        watcher->collisionDetector->handleFileChange(filename, events);
     }
 }
-
-// Sample usage
-/*
-int main() {
-    FileWatcher watcher("/path/to/directory", [](const std::string& filename) {
-        std::cout << "File changed: " << filename << std::endl;
-    });
-
-    watcher.start();
-    // Do other work
-    watcher.stop();
-    return 0;
-}
-*/
